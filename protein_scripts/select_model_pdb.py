28d82e2f0f8406168910397ca360309996782b94
Tristyn Alxander <Trystyn.Alxander@gmail.com>
2018-03-21 14:51:20 -0400

    Replace NMR select bash script with python

---
+++ protein_scripts/select_model_pdb.py
@@ -0,0 +1,43 @@
+# Imports
+import pars_pdb
+
+# Imports for Main
+if (__name__ == "__main__"):
+    import os
+    import sys
+
+
+def select_model(pdb_path,m):
+    
+    # Initialize Files
+    pdb_old = open(pdb_path)                                            # Get  old PDB File
+    pdb_new = open(pdb_path[:-4]+"_m"+str(m)+".pdb","w+")              # Make new PDB File
+    
+    # Initialize Variables
+    model_number = 0
+    
+    for line in pdb_old:
+        fields = line.strip().split()                                   # Splits Columns
+        if(fields[0] == "ATOM" or fields[0] == "HETATM"):               #
+            
+            if(model_number == m):                                      # If you're on the correct model
+                pdb_new.write(line)                                     # Write the rows
+            
+        elif(fields[0] == "TER"):
+            
+            if(model_number == m):                                      # If you're on the correct model
+                pdb_new.write(line)                                     # Write the rows
+            
+        elif(fields[0] == "MODEL"):
+            model_number += 1
+        elif(fields[0] == "ENDMDL"):
+            continue
+        else: 
+            pdb_new.write(line)                 # Don't mess with non Atom non Model Rows 
+    
+    pdb_old.close()
+    pdb_new.close()
+if (__name__ == "__main__"):
+    pdb_path = os.path.abspath(sys.argv[1])     # Get PDB
+    m        = int(sys.argv[2])                 # Get PDB
+    select_model(pdb_path,m)
