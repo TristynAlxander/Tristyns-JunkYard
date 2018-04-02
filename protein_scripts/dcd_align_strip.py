a5f8f25fc4b16c00d1538d6234aafdbe1eca33c1
Tristyn Alxander <Trystyn.Alxander@gmail.com>
2018-04-02 10:51:29 -0400

    Update dcd_align_strip.py

--- protein_scripts/dcd_align_strip.py
+++ protein_scripts/dcd_align_strip.py
@@ -8,40 +8,48 @@ if (__name__ == "__main__"):
     import os
 
 
-def save_dcd(uni, filename, sel_str="protein"):
+def save_dcd(uni, filename, sel_str="not segid SOLV"):
+    dcd = filename[:-4]+"_new.dcd"
     sel = uni.select_atoms(sel_str)                                                         # Make Selection
-    with MDAnalysis.Writer(filename, multiframe=True, n_atoms=sel.n_atoms) as W:      # Make Writer
+    with MDAnalysis.Writer(dcd, multiframe=False, n_atoms=sel.n_atoms) as W:                 # Make Writer
         for ts in uni.trajectory:                                                           # For each time-step
             W.write(sel)                                                                    # Save Selected
 
 def align_traj(traj_uni, ref_uni, sel_str="name CA"):
     # Get Selection
-    sel_ref = ref.select_atoms(sel_str)
+    sel_ref = ref_uni.select_atoms(sel_str)
 
     # Align the trajectory to reference in memory.
-    alignment = align.AlignTraj(traj_uni, ref_uni, in_memory=True, select=sel_str)
+    alignment = align.AlignTraj(traj_uni, ref_uni, in_memory=False, select=sel_str)
     alignment.run()
 
     # Return Aligned trajectory in traj.
     return traj_uni
 
 if (__name__ == "__main__"):
+    sel_str="not segid SOLV"
     
     # sys.argv[0] is this script, so don't count it.
     if(len(sys.argv) == 3):
-        psf      = os.path.abspath(sys.argv[1])     # 
-        dcd      = os.path.abspath(sys.argv[2])     #
+        psf  = os.path.abspath(sys.argv[1])             # 
+        dcd  = os.path.abspath(sys.argv[2])             # 
+        path = os.path.dirname(dcd)+"/"                 # 
+        pdb  = sys.argv[2][:-4]+"_new.pdb"              # 
         
-        traj_uni = MDAnalysis.Universe(psf, dcd)
-        
+        traj_uni = MDAnalysis.Universe(psf, dcd)        # 
+        sel = traj_uni.select_atoms(sel_str).write(pdb) # 
+        ref_uni  = MDAnalysis.Universe(path+pdb)        # 
+       
     if(len(sys.argv) == 4):
         psf      = os.path.abspath(sys.argv[1])     # 
-        dcd      = os.path.abspath(sys.argv[2])     #
-        pdb_ref  = os.path.abspath(sys.argv[3])     #
+        dcd      = os.path.abspath(sys.argv[2])     # 
+        pdb_ref  = os.path.abspath(sys.argv[3])     # 
         
-        traj_uni = MDAnalysis.Universe(psf, dcd)
-        ref_uni  = MDAnalysis.Universe(pdb_ref)
-        traj_uni = align_traj(traj_uni,ref_uni)
+        traj_uni = MDAnalysis.Universe(psf, dcd)    # 
+        ref_uni  = MDAnalysis.Universe(pdb_ref)     # 
+        traj_uni = align_traj(traj_uni,ref_uni)     # 
     
-    save_dcd(traj_uni, sys.argv[2][:-4]+"_new"+sys.argv[2][-4:])
+    traj_uni = align_traj(traj_uni,ref_uni)
+    
+    save_dcd(traj_uni, sys.argv[2],sel_str=sel_str)
     
