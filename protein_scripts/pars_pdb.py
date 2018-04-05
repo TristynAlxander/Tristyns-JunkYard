b34720af0de912785310b0d6d8cfc35ceb51cf81
Tristyn Alxander <Trystyn.Alxander@gmail.com>
2018-04-05 09:39:32 -0400

    Update pars_pdb.py

--- protein_scripts/pars_pdb.py
+++ protein_scripts/pars_pdb.py
@@ -32,21 +32,21 @@
 # ATOM
 def line_to_pdb_atom(line):
     fields = []
-    fields.append(line[0:6].strip())        # 0  ATOM
-    fields.append(line[6:11].strip())       # 1  Atom Serial Number
-    fields.append(line[12:16].strip())      # 2  Atom Name
-    fields.append(line[16].strip())         # 3  Alternate Location Indicator
-    fields.append(line[17:21].strip())      # 4  Residue Name
-    fields.append(line[21].strip())         # 5  Chain Identifier
-    fields.append(line[22:26].strip())      # 6  Residue Sequence Number
-    fields.append(line[26].strip())         # 7  Insertion Codes of Residues
-    fields.append(line[30:38].strip())      # 8  X Orthogonal A Coordinate
-    fields.append(line[38:46].strip())      # 9  Y Orthogonal A Coordinate
-    fields.append(line[46:54].strip())      # 10 Z Orthogonal A Coordinate
-    fields.append(line[54:60].strip())      # 11 Occupancy
-    fields.append(line[60:66].strip())      # 12 Temperature Factor
-    fields.append(line[72:76].strip())      # 13 Segment Identifier
-    fields.append(line[76:78].strip())      # 14 Element Symbol
+    fields.append(line[0:6].strip())               # 0  ATOM
+    fields.append(int(line[6:11].strip()))         # 1  Atom Serial Number
+    fields.append(line[12:16].strip())             # 2  Atom Name
+    fields.append(line[16].strip())                # 3  Alternate Location Indicator
+    fields.append(line[17:21].strip())             # 4  Residue Name
+    fields.append(line[21].strip())                # 5  Chain Identifier
+    fields.append(int(line[22:26].strip()))        # 6  Residue Sequence Number
+    fields.append(line[26].strip())                # 7  Insertion Codes of Residues
+    fields.append(float(line[30:38].strip()))      # 8  X Orthogonal A Coordinate
+    fields.append(float(line[38:46].strip()))      # 9  Y Orthogonal A Coordinate
+    fields.append(float(line[46:54].strip()))      # 10 Z Orthogonal A Coordinate
+    fields.append(float(line[54:60].strip()))      # 11 Occupancy
+    fields.append(float(line[60:66].strip()))      # 12 Temperature Factor
+    fields.append(line[72:76].strip())             # 13 Segment Identifier
+    fields.append(line[76:78].strip())             # 14 Element Symbol
     return fields
 def print_atom_fields(fields):
     print("{0:<30.30}{1}".format("ATOM: "               , fields[0]))        # 0  ATOM
@@ -71,12 +71,12 @@ def pdb_atom_to_line(fields):
 # TER
 def line_to_pdb_ter(line):
     fields = []
-    fields.append(line[0:3].strip())        # 0  TER
-    fields.append(line[6:11].strip())       # 1  Atom Serial Number
-    fields.append(line[17:20].strip())      # 2  Residue Name
-    fields.append(line[21].strip())         # 3  Chain Identifier
-    fields.append(line[22:26].strip())      # 4  Residue Sequence Number
-    fields.append(line[26].strip())         # 5  Insertion Codes of Residues
+    fields.append(line[0:3].strip())             # 0  TER
+    fields.append(int(line[6:11].strip()))       # 1  Atom Serial Number
+    fields.append(line[17:20].strip())           # 2  Residue Name
+    fields.append(line[21].strip())              # 3  Chain Identifier
+    fields.append(int(line[22:26].strip()))      # 4  Residue Sequence Number
+    fields.append(line[26].strip())              # 5  Insertion Codes of Residues
     return fields
 def pdb_ter_to_line(fields):
     line = (
